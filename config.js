d63a35e2dc2de8a31f47afa16004580701d2d0f3
Zartasha Kanwal <zartashakanwal529@gmail.com>
2025-04-18 17:35:03 +0500

    Initial backend commit

---
+++ config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://zartashakanwal529:zari.147@book-store-mern.sf7vlqc.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-store-MERN';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
